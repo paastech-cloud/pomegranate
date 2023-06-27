fa1024f9246bd52924854a56028fa0ff3d445457
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2023-06-27 12:18:05 +0200

    chore: clean up main

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,9 @@ mod engine;
 mod errors;
 
 use log::info;
-use std::collections::HashMap;
-use std::thread;
-use std::time::Duration;
 
 use crate::application::Application;
 use crate::engine::docker_engine::DockerEngine;
-use crate::engine::Engine;
 
 #[tokio::main]
 async fn main() {
@@ -27,52 +23,4 @@ async fn main() {
 
     // Init the Docker engine
     let engine = DockerEngine::new();
-
-    // TODO: Remove me
-    // Start a sample application
-    let sample_app = Application {
-        application_id: String::from("webapp"),
-        project_id: String::from("test"),
-        image_name: String::from("nginx"),
-        image_tag: String::from("latest"),
-        env_variables: HashMap::from([(String::from("VERBOSITY"), String::from("5"))]),
-    };
-
-    engine
-        .start_application(&sample_app)
-        .await
-        .unwrap_or_default();
-    info!(
-        "is app running? {}",
-        engine
-            .is_application_running(&sample_app.project_id, &sample_app.application_id)
-            .await
-            .unwrap()
-    );
-    thread::sleep(Duration::from_secs(3));
-
-    engine
-        .restart_application(&sample_app)
-        .await
-        .unwrap_or_default();
-    info!(
-        "is app running? {}",
-        engine
-            .is_application_running(&sample_app.project_id, &sample_app.application_id)
-            .await
-            .unwrap()
-    );
-    thread::sleep(Duration::from_secs(3));
-
-    engine
-        .stop_application(&sample_app.project_id, &sample_app.application_id)
-        .await
-        .unwrap_or_default();
-    info!(
-        "is app running? {}",
-        engine
-            .is_application_running(&sample_app.project_id, &sample_app.application_id)
-            .await
-            .unwrap()
-    );
 }
