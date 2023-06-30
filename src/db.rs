5f1a4e71f127ef617bfc4e88c67221b5486de17a
Adrien Raimbault <adriexraimbault@hotmail.com>
2023-06-30 21:16:39 +0200

    feat(db): change sync postgres to async tokio_postgres

--- src/db.rs
+++ src/db.rs
@@ -1,42 +1,48 @@
-use postgres::{Client, Error, NoTls};
+use tokio_postgres::{Client, Error, NoTls};
+
+use crate::config::DatabaseConfig;
 
 pub struct Database {
-    user: String,
-    pass: String,
-    url: String,
-    port: String,
+    client: Client,
 }
 
 impl Database {
-    pub fn new(user: String, pass: String, url: String, port: String) -> Database {
-        Database {
-            user,
-            pass,
-            url,
-            port,
-        }
-    }
-    pub fn connect(&self) -> Result<(), Error> {
+    pub async fn new() -> Self {
+        let configuration = DatabaseConfig::new();
+
         // Connect to postgresql client
         let connection_link = format!(
             "postgresql://{}:{}@{}:{}/pomegranate",
-            self.user, self.pass, self.url, self.port
+            configuration.db_user,
+            configuration.db_passwd,
+            configuration.db_url,
+            configuration.db_port
         );
-        println!("{:?}", connection_link);
 
-        let mut client = Client::connect(&connection_link, NoTls)?;
+        let (client, connection) = tokio_postgres::connect(&connection_link, NoTls).await.unwrap();
 
-        // Create table as an example
-        client.batch_execute(
-            "
-            CREATE TABLE IF NOT EXISTS accounts (
-                user_id serial PRIMARY KEY,
-                username VARCHAR ( 50 ) UNIQUE NOT NULL,
-                password VARCHAR ( 50 ) NOT NULL,
-                email VARCHAR ( 255 ) UNIQUE NOT NULL
-            );",
-        )?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
 
-        Ok(())
+        return Database { client: client };
+    }
+
+    pub async fn create_table_accounts(&mut self) -> Result<(), Error> {
+        self.client
+            .query(
+                "CREATE TABLE SALUTE (
+            user_id serial PRIMARY KEY,
+            username VARCHAR ( 50 ) UNIQUE NOT NULL,
+            password VARCHAR ( 50 ) NOT NULL,
+            email VARCHAR ( 255 ) UNIQUE NOT NULL
+        );",
+                &[],
+            )
+            .await?;
+
+        return Ok(());
     }
 }
