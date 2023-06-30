eea3e6cefc6f62d9d2ebddac4c97fa19b5b91909
Adrien Raimbault <adriexraimbault@hotmail.com>
2023-06-30 20:22:49 +0200

    feat(db): add database connection and test to project

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 mod application;
+mod db;
 mod engine;
 
+use dotenv::dotenv;
 use log::info;
 
 use crate::application::Application;
+use crate::db::Database;
 use crate::engine::docker_engine::DockerEngine;
 
 #[tokio::main]
 async fn main() {
+    // Load env variables
+    dotenv().ok();
+
+    // Setting up the connection to postgres db
+    let postgres_db_user =
+        std::env::var("POMEGRANATE_DB_USER").expect("POMEGRANATE_DB_USER must be set.");
+    let postgres_db_pass =
+        std::env::var("POMEGRANATE_DB_PWD").expect("POMEGRANATE_DB_PWD must be set.");
+    let postgres_db_url =
+        std::env::var("POMEGRANATE_DB_URL").expect("POMEGRANATE_DB_URL must be set.");
+    let postgres_db_port =
+        std::env::var("POMEGRANATE_DB_PORT").expect("POMEGRANATE_DB_PORT must be set.");
+
     // Start the application
     env_logger::builder()
         .filter(None, log::LevelFilter::Info)
@@ -22,4 +38,12 @@ async fn main() {
 
     // Init the Docker engine
     let engine = DockerEngine::new();
+
+    let database = Database::new(
+        postgres_db_user,
+        postgres_db_pass,
+        postgres_db_url,
+        postgres_db_port,
+    );
+    println!("{:?}", database.connect());
 }
