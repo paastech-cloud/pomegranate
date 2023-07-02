8cc878e50d9f903eaf65d194fe5156f4a58a7d14
remi-espie <remi.espie@etu.umontpellier.fr>
2023-07-02 20:04:44 +0200

    feat(db): add struct for Deployment and Project independently and makes it transparent for grpc server

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,5 @@
 use crate::application::Application;
+use chrono::{Utc};
 use std::collections::HashMap;
 use std::fmt::Error;
 
@@ -9,11 +10,8 @@ impl Db {
         Db {}
     }
 
-    pub fn get_deployment_join_project(
-        &self,
-        uuid: String,
-    ) -> Result<DeploymentJoinProject, Error> {
-        DeploymentJoinProject::new(uuid)
+    pub fn get_deployment_and_project(&self, uuid: String) -> Result<DeploymentAndProject, Error> {
+        DeploymentAndProject::new(uuid)
     }
 
     pub async fn set_deployment_config(&self, _uuid: String, _config: String) -> Result<(), Error> {
@@ -27,11 +25,11 @@ impl Db {
     /// # Returns
     /// Nothing, wrapped in a result.
     pub fn get_app(&self, uuid: String) -> Result<Application, Error> {
-        match self.get_deployment_join_project(uuid.clone()) {
-            Ok(deployment) => Ok(Application {
-                application_id: uuid.clone(),
-                project_id: deployment.project_uuid,
-                image_name: format!("{}/{}", deployment.user_id, uuid),
+        match self.get_deployment_and_project(uuid) {
+            Ok(app) => Ok(Application {
+                application_id: app.deployment.uuid.clone(),
+                project_id: app.project.uuid,
+                image_name: format!("{}/{}", app.project.user_id, app.deployment.uuid),
                 image_tag: String::from("latest"),
                 ..Default::default()
             }),
@@ -51,11 +49,11 @@ impl Db {
         uuid: String,
         hashmap_config: HashMap<String, String>,
     ) -> Result<Application, Error> {
-        match self.get_deployment_join_project(uuid.clone()) {
+        match self.get_deployment_and_project(uuid) {
             Ok(app) => Ok(Application {
-                application_id: uuid.clone(),
-                project_id: app.project_uuid.clone(),
-                image_name: format!("{}/{}", app.user_id, uuid),
+                application_id: app.deployment.uuid.clone(),
+                project_id: app.project.uuid,
+                image_name: format!("{}/{}", app.project.user_id, app.deployment.uuid),
                 image_tag: String::from("latest"),
                 env_variables: hashmap_config,
             }),
@@ -64,23 +62,47 @@ impl Db {
     }
 }
 
-pub struct DeploymentJoinProject {
+pub struct Deployment {
     pub uuid: String,
     pub name: String,
     pub config: String,
+    pub created_at: String,
+    pub updated_at: String,
     pub project_uuid: String,
+}
+
+pub struct Project {
+    pub uuid: String,
+    pub name: String,
+    pub created_at: String,
+    pub updated_at: String,
     pub user_id: String,
 }
 
-impl DeploymentJoinProject {
-    pub fn new(uuid: String) -> Result<DeploymentJoinProject, Error> {
+pub struct DeploymentAndProject {
+    pub project: Project,
+    pub deployment: Deployment,
+}
+
+impl DeploymentAndProject {
+    pub fn new(uuid: String) -> Result<DeploymentAndProject, Error> {
         if uuid == "nginx" {
-            Ok(DeploymentJoinProject {
-                uuid,
-                name: String::from("nginx"),
-                config: String::from(""),
-                project_uuid: String::from("test"),
-                user_id: String::from("test"),
+            Ok(DeploymentAndProject {
+                project: Project {
+                    uuid: String::from("uuid_project"),
+                    name: String::from("name_project"),
+                    created_at: Utc::now().to_string(),
+                    updated_at: Utc::now().to_string(),
+                    user_id: String::from("user_project"),
+                },
+                deployment: Deployment {
+                    uuid: String::from("nginx"),
+                    name: String::from("nginx"),
+                    config: String::from(""),
+                    created_at: Utc::now().to_string(),
+                    updated_at: Utc::now().to_string(),
+                    project_uuid: String::from("uuid_project"),
+                },
             })
         } else {
             Err(Error)
